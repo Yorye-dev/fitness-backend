a5f560316fdb2bbf6ebd74d37bd22d18127e6e67
Yorye <jorge.abrodes@gmail.com>
2025-08-19 21:42:41 +0200

    feaat: Add user at model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,18 +1,17 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use chrono::{DateTime, Utc};
 
 use crate::enums::activity_level;
 
-#[derive(sqlx::FromRow)]
+#[derive(sqlx::FromRow, Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
-    pub email: String,
     pub password_hash: String,
+    pub sex: String,
     pub weight: f32,
     pub height: i32,
     pub age: i32,
     pub activity_level: activity_level::ActivityLevel,
-    pub created_at: DateTime<Utc>,
+    
 }
