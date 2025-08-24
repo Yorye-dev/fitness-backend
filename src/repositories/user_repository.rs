e68e244921c0dc5a7f78e14d04f524e83c5c66b1
Yorye <jorge.abrodes@gmail.com>
2025-08-24 22:41:55 +0200

    feat: firts repo, version

--- src/repositories/user_repository.rs
+++ src/repositories/user_repository.rs
@@ -1,1 +1,24 @@
+use sqlx::{Pool, Postgres};
+use crate::models::user::User;
+use uuid::Uuid;
 
+pub struct UserRepository<'a> {
+    pool: &'a Pool<Postgres>,
+}
+
+impl<'a> UserRepository<'a> {
+
+    pub async fn create_user(&self, user: &User) -> Result<User, sqlx::Error> {
+        let rec sqlx::query_as::<_, User>(
+            r#"
+            INSERT INTO users (id, username, email, password_hash, age, sex, height_cm, weight_kg, activity_level)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            RETURNING *
+            "#,
+        )
+        .bind(&user.username)
+        .bind(&user.email)
+        .fetch_one(self.pool)
+        .await
+    }
+}
