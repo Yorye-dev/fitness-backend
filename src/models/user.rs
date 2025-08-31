3aeea755a1170dc57e73a3d7e871e53268d08376
Yorye <jorge.abrodes@gmail.com>
2025-08-31 22:45:49 +0200

    feat: add a public User model

--- src/models/user.rs
+++ src/models/user.rs
@@ -11,3 +11,28 @@ pub struct User {
     pub age: i32,
     pub activity_level: String,   
 }
+
+#[derive(sqlx::FromRow, Serialize, Deserialize, Debug)]
+pub struct PublicUser {
+    pub id: String,
+    pub username: String,
+    pub sex: String,
+    pub weight: f32,
+    pub height: i32,
+    pub age: i32,
+    pub activity_level: String,   
+}
+
+impl From<User> for PublicUser {
+    fn from(user: User) -> Self {
+        PublicUser {
+            id: user.id,
+            sex: user.sex,
+            username: user.username,
+            weight: user.weight,
+            height: user.height,
+            age: user.age,
+            activity_level: user.activity_level,
+        }
+    }
+}
