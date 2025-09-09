16f816342d90ad07b2cabd2498d8ecb4d739db99
Yorye <jorge.abrodes@gmail.com>
2025-09-09 18:51:32 +0200

    feat: 001-implement, RegisterUserDto

---
+++ src/dtos/register_user_dto.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(sqlx::FromRow, Serialize, Deserialize, Debug)]
+pub struct RegisterUserDto {
+    pub username: String,
+    pub plain_password: String,
+    pub sex: String,
+    pub weight: f32,
+    pub height: i32,
+    pub age: i32,
+    pub activity_level: String,   
+}
