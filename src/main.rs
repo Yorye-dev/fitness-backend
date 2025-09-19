c838ecaed5833ca21e212fa0317a58fb07e6100f
Yorye <jorge.abrodes@gmail.com>
2025-09-19 22:17:06 +0200

    feat: 001 - add register handler and register router

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,19 @@ async fn main() {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("Error en .env falta DATABASE_URL");
+    let app_url = env::var("APP_URL").expect("Error en .env falta APP_URL");
+    let project_name = env::var("PROJECT_NAME").expect("Error en .env falta PROJECT_NAME");
 
     let _pool = init_db_pg_pool(&database_url).await.unwrap();
+    let services = Services::new(_pool); // Estructura de datos, de los servicios
+    
+    println!("{} corriendo en: {}", project_name, app_url);
 
-    let user_service = Services::new(_pool).user_service; // Structura de datos, 
+    let app = routes::app_routes(services.clone());
+    let listener = tokio::net::TcpListener::bind(app_url).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
+    /*
     let register_user_dto = RegisterUserDto {
         username: "Paco".into(),
         plain_password: "123".into(),
@@ -38,10 +46,9 @@ async fn main() {
         activity_level: activity_level::ActivityLevel::Sedentary.as_string()
     };
 
-
-    
-    user_service.register_user(register_user_dto).await;
-
+    //services.user_service.register_user(register_user_dto).await;
+    //
+    //TODO: Implemenatar esta logica en capa de servicios.
     println!("Introduce la altura: ");
     io::stdout().flush().unwrap();
     let mut height = String::new();
@@ -64,4 +71,5 @@ async fn main() {
     
     //config
     println!("El tmp es: {}", utils::metrics::calculate_tdee(weight, height, age))
+    */
 }
