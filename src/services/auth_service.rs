cf0a05d4df767f2f384f7a30fa5aeff12e36a3b5
Yorye <jorge.abrodes@gmail.com>
2025-09-23 16:26:05 +0200

    NOT WORK

--- src/services/auth_service.rs
+++ src/services/auth_service.rs
@@ -1,6 +1,7 @@
 use crate::repositories::user_repository::UserRepository;
 use crate::factories::user_factory::UserFactory;
 use crate::dtos::login_user_dto::LoginUserDto;
+use crate::errors::AuthError;
 
 #[derive(Clone)]
 pub struct AuthService {
@@ -16,8 +17,10 @@ impl AuthService {
 
     pub async fn login_user(&self, dto :LoginUserDto) {
 
-        let user = self.repo.get_public_user_by_username(&dto.username).await;
-
-
+        let user = self.repo
+        .get_user_by_username(&dto.username)
+        .await
+        .map_err(|_| AuthError::DatabaseError)
+        .ok_or(AuthError::UserNotFound);
     }
 }
