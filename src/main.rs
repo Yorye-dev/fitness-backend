a692466d6385d38a58316ff6d5051f0c1853c52a
Yorye <jorge.abrodes@gmail.com>
2026-07-16 21:13:32 +0200

    003 - Add cors

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ pub mod application;
 pub mod presentation;
 pub mod shared;
 pub mod infrastructure;
+use axum::http::{HeaderValue, Method};
+use tower_http::cors::{Any, CorsLayer};
 
 use dotenv::dotenv;
 use services::Services;
@@ -18,6 +20,21 @@ async fn main() {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("Error en .env falta DATABASE_URL");
+    let cors = CorsLayer::new()
+    .allow_origin(
+        "http://localhost:5173"
+            .parse::<HeaderValue>()
+            .unwrap(),
+    )
+    .allow_methods([
+        Method::GET,
+        Method::POST,
+        Method::PUT,
+        Method::PATCH,
+        Method::DELETE,
+        Method::OPTIONS,
+    ])
+    .allow_headers(Any);
     let app_url = env::var("APP_URL").expect("Error en .env falta APP_URL");
     let project_name = env::var("PROJECT_NAME").expect("Error en .env falta PROJECT_NAME");
     let secret_key = env::var("SECRET_KEY").expect("Error en .env falta SECRET_KEY");
@@ -27,7 +44,7 @@ async fn main() {
     
     println!("{} corriendo en: {}", project_name, app_url);
 
-    let app = presentation::routes::app_routes(services.clone());
+    let app = presentation::routes::app_routes(services.clone()).layer(cors);
     let listener = tokio::net::TcpListener::bind(app_url).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
